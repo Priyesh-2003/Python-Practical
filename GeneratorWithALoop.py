03724b021f0cd9e5c7ea706f23db8494b526d581
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:49:48 +0530

    Create GeneratorWithALoop.py

---
+++ GeneratorWithALoop.py
@@ -0,0 +1,12 @@
+def count_up_to(n):
+    """Generator that yields numbers from 1 to n."""
+    i = 1
+    while i <= n:
+        yield i
+        i += 1
+
+# Using the generator with a loop
+for number in count_up_to(5):
+    print(number)
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
