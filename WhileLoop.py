45de2688de7d365c888e3cf85bb0b3ebd697f13f
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 10:08:06 +0530

    Create WhileLoop.py

---
+++ WhileLoop.py
@@ -0,0 +1,9 @@
+print("Demonstration of While Loop:")
+# Print numbers from 1 to 5 using while loop
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1  # increase the value of i in each loop
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
