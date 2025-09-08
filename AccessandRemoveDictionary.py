6a2ddd0e4ccbae1a09161eb81d7368894b7374da
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:38:46 +0530

    Create AccessandRemoveDictionary.py

---
+++ AccessandRemoveDictionary.py
@@ -0,0 +1,9 @@
+#write a python to access and remove dictionary elements
+
+my_dict = {'name':'bob','age':30,'job':'Engineer'}
+
+name = my_dict.get('name')
+
+print(name)
+
+print("\nThis program is written and executed by Mannan Tayal (0231BCA047)")
