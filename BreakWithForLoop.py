d9d8e222448274064eeed2f767073da3ff35aa06
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:41:22 +0530

    Create BreakWithForLoop.py

---
+++ BreakWithForLoop.py
@@ -0,0 +1,9 @@
+print("Demobstration of Break statement with For Loop:")
+# Print numbers from 1 to 10, but stop when number is 6
+
+for i in range(1, 11):
+    if i == 6:
+        break  # Exit the loop when i is 6
+    print(i)
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
