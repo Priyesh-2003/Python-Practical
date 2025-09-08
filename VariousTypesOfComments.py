bed40386a2c2bcbb3b310d1c3938ca90906d0d37
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 10:07:37 +0530

    Create VariousTypesOfComments.py

---
+++ VariousTypesOfComments.py
@@ -0,0 +1,16 @@
+# This is a single-line comment
+
+"""
+This is a multi-line comment
+written using triple double quotes.
+It can span multiple lines.
+"""
+
+'''
+This is another multi-line comment
+using triple single quotes.
+'''
+
+print("Comments are not shown in output")
+
+print("this program was written and executed by Mannan Tayal (0231BCA047)")
