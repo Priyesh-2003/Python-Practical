fffcb136a774110f650b622c8807114269bb768b
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:52:52 +0530

    Create InputMethod.py

---
+++ InputMethod.py
@@ -0,0 +1,8 @@
+# Taking user input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello", name + "!")
+print("You are", age, "years old.")
+
+print("this program was written and executed by Mannan Tayal (0231BCA047)")
