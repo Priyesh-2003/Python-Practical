c1d24ab5e0c28c7697e58c7c73511537043ba5de
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 10:23:50 +0530

    Update MutipleInputsPassedByCommas.py

--- MutipleInputsPassedByCommas.py
+++ MutipleInputsPassedByCommas.py
@@ -6,4 +6,4 @@ numbers = list(map(int, input("Enter numbers separated by commas: ")
 # Printing the list of numbers
 print("List of numbers:", numbers)
 
-print("This program is written and executed by Mannan Tayal (0231BCA047)")
+print("This program is written and executed by priyesh(0231BCA024)")
