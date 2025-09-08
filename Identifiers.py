d6f115e0d9cc45af1813a77270213c445834f989
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:50:36 +0530

    Create Identifiers.py

---
+++ Identifiers.py
@@ -0,0 +1,16 @@
+# Valid identifiers
+name = "Mannan"
+age = 20
+
+print("Valid Identifiers:")
+print("name =", name)
+print("age =", age)
+
+# Invalid identifiers (these would cause errors if we tried to use them)
+# 2name = "wrong"
+# my name = "error"
+# class = "oops"
+
+print("\nInvalid Identifiers: 2name, my name, class")
+
+print("this program was written and executed by Mannan Tayal (0231BCA047)")
