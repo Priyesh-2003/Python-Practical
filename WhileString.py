b01b5820ee5fece9f3b15132be063436d1f1117b
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 10:08:34 +0530

    Create WhileString.py

---
+++ WhileString.py
@@ -0,0 +1,11 @@
+print("Demonstration of While String:")
+# Using while loop to print each character of a string
+
+text = "Python"
+i = 0
+
+while i < len(text):
+    print("Character at index", i, "is", text[i])
+    i += 1
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
