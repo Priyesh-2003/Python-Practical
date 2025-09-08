40c40b6032125f8ca2a273074e482f622a3c7aea
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:45:08 +0530

    Create CoreObjects&Function.py

---
+++ CoreObjects&Function.py
@@ -0,0 +1,28 @@
+# Text and number
+name = "Lily"
+age = 8
+print(name)
+print(age)
+
+# A list (a group of items)
+colors = ["red", "blue", "green"]
+print(colors)
+print(len(colors))  # Count how many
+
+# A dictionary (like a mini info card)
+person = {"name": "Lily", "age": 8}
+print(person["name"])
+print(person["age"])
+
+# A set (no repeats)
+nums = [1, 1, 2]
+print(set(nums))
+
+# Add numbers
+marks = [10, 20, 30]
+print(sum(marks))
+print(max(marks))
+
+# Say each color
+for color in colors:
+    print(color)
