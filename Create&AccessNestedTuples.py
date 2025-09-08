781a66513e6f20ac372939526855a9008644a432
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:45:49 +0530

    Create Create&AccessNestedTuples.py

---
+++ Create&AccessNestedTuples.py
@@ -0,0 +1,9 @@
+# write a python to code create and access nested tuples
+
+nested_tuple = (1,(2,3),4)
+
+print(nested_tuple[1][0])
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
+
+#
