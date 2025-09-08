bafdaff9c2257f6c88d7213b66142166b5910c37
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 10:27:57 +0530

    Update UsingIteratorsinloop.py

--- UsingIteratorsinloop.py
+++ UsingIteratorsinloop.py
@@ -7,4 +7,4 @@ it = iter(my_list)
 for item in it:
     print(item)
     
-print("\nThis program is written and executed by Mannan Tayal (0231BCA047)")
+print("\nThis program is written and executed by priyesh (0231BCA024)")
