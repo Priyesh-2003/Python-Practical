e1cbeda5ca7edb6eb04c52344e482bab7e8c2219
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 10:07:09 +0530

    Create UsingIteratorsinloop.py

---
+++ UsingIteratorsinloop.py
@@ -0,0 +1,10 @@
+print("Program to USE Iterators in a Loop:\n")
+
+my_list = [1,2,3,4]
+
+it = iter(my_list)
+
+for item in it:
+    print(item)
+    
+print("\nThis program is written and executed by Mannan Tayal (0231BCA047)")
