930d9c78ba8906b9c0d10f40dc517b81ef0fbe67
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:46:17 +0530

    Create CreateAndAccessTuples.py

---
+++ CreateAndAccessTuples.py
@@ -0,0 +1,9 @@
+#write a python code to create and access tuples
+
+my_tuples = (1,2,3,4)
+
+print(my_tuples[0])
+
+print(my_tuples[-1])
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
