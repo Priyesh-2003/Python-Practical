bb24eeb054f388c0d951f4040785551f5c07db42
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:48:18 +0530

    Create DateTypesAndVariables.py

---
+++ DateTypesAndVariables.py
@@ -0,0 +1,13 @@
+# Different types of variables with different data types
+name = "Mannan"         # string
+age = 19                # integer
+height = 6.2            # float
+is_student = True       # boolean
+
+# Displaying the values and their data types
+print("Name:", name, "| Type:", type(name))
+print("Age:", age, "| Type:", type(age))
+print("Height:", height, "| Type:", type(height))
+print("Is Student:", is_student, "| Type:", type(is_student))
+
+print("this program was written and executed by Mannan Tayal (0231BCA047)")
