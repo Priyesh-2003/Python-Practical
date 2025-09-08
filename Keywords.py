90c2f6613c0c9512c2be4a3e60c4d059ece3b79b
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 10:09:29 +0530

    Create Keywords.py

---
+++ Keywords.py
@@ -0,0 +1,10 @@
+import keyword
+
+# Display all Python keywords
+print("Python Keywords:")
+print(keyword.kwlist)
+
+# Count of keywords
+print("\nTotal number of keywords:", len(keyword.kwlist))
+
+print("this program was written and executed by Mannan Tayal (0231BCA047)")
