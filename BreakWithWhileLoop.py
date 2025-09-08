4ac59d08e2c5fe44972b7ba60ee4d6a5eeede56d
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:41:49 +0530

    Create BreakWithWhileLoop.py

---
+++ BreakWithWhileLoop.py
@@ -0,0 +1,11 @@
+print("Demonstration of Break statement with While Loop:")
+# Print numbers from 1 to 10, but stop when number is 6
+
+i = 1
+while i <= 10:
+    if i == 6:
+        break  # Exit the loop when i is 6
+    print(i)
+    i += 1
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
