2469bf8fa88eac5c1851a9d4efdf465e8857dfa8
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 10:06:49 +0530

    Create TryExceptFinally.py

---
+++ TryExceptFinally.py
@@ -0,0 +1,16 @@
+print("Demonstration of Try and Except and Finally:")
+# Demonstration of try, except, and finally
+
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    result = num1 / num2
+    print("Result:", result)
+except ZeroDivisionError:
+    print("Error: Cannot divide by zero!")
+except ValueError:
+    print("Error: Please enter valid integers!")
+finally:
+    print("This block always runs (finally block).")
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
