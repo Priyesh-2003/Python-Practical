fbe5106a6dede59da608455d78e4b9dcf0162cda
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 10:22:11 +0530

    Update IterateThroughDictItems.py

--- IterateThroughDictItems.py
+++ IterateThroughDictItems.py
@@ -5,4 +5,4 @@ my_dict = {'a':1,'b':2,'c':3}
 for key,value in my_dict.items():
     print(f"{key}:{value}")
     
-print("\nThis program is written and executed by Mannan Tayal (0231BCA047)")
+print("\nThis program is written and executed by Priyesh (0231BCA024)")
