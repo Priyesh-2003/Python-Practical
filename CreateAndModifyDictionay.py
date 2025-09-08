3a1e027ec69bf074e9020f71845b6675254193a9
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:46:47 +0530

    Create CreateAndModifyDictionay.py

---
+++ CreateAndModifyDictionay.py
@@ -0,0 +1,13 @@
+#write a python code to create and modify a dictionary
+
+my_dict = {'name':'Alice','age':25}
+
+my_dict ['age'] = 26
+
+my_dict ['city'] = 'New York'
+
+print(my_dict)
+
+print("\nThis program is written and executed by Mannan Tayal (0231BCA047)")
+
+#
