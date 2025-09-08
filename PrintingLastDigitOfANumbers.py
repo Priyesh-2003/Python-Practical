500cbcdc1b47fa24c9884cf1a1d88d4f18cc5cd3
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 10:01:43 +0530

    Create PrintingLastDigitOfANumbers.py

---
+++ PrintingLastDigitOfANumbers.py
@@ -0,0 +1,11 @@
+print("Printing The Last Digit of a given Integer Number:")
+# Taking input from the user
+num = int(input("Enter an integer number: "))
+
+# Getting the last digit using modulus operator
+last_digit = num % 10
+
+# Printing the last digit
+print("Last digit:", last_digit)
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
