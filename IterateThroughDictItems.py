ac5940866e9a733a5987b267bebb062c082da7da
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:53:32 +0530

    Create IterateThroughDictItems.py

---
+++ IterateThroughDictItems.py
@@ -0,0 +1,8 @@
+print("Program to Iterate Through Dictionary Items:\n")
+
+my_dict = {'a':1,'b':2,'c':3}
+
+for key,value in my_dict.items():
+    print(f"{key}:{value}")
+    
+print("\nThis program is written and executed by Mannan Tayal (0231BCA047)")
