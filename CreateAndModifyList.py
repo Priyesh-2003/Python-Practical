b31007868db4933cc9b1104c9cce1454c4a7f76f
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:47:15 +0530

    Create CreateAndModifyList.py

---
+++ CreateAndModifyList.py
@@ -0,0 +1,15 @@
+#write a python code to create and modify a list.
+
+my_list = [1,2,3,4,5]
+
+my_list.append(6)
+
+my_list.insert(2,2.5)
+
+my_list.remove(4)
+
+print(my_list)
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
+
+#
