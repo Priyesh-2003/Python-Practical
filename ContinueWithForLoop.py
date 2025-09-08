ad97df4d5e98bc4b862a76440f9093fc7952f7be
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:43:56 +0530

    Create ContinueWithForLoop.py

---
+++ ContinueWithForLoop.py
@@ -0,0 +1,9 @@
+print("Demonstration of Continue Statement with For loop:")
+# Print all numbers from 1 to 10, except 5
+
+for i in range(1, 11):
+    if i == 5:
+        continue  # Skip the number 5
+    print(i)
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
