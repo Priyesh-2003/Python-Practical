8339d59889f01e2be0f4706a59705173a7a4545e
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 10:02:31 +0530

    Create SetOperations.py

---
+++ SetOperations.py
@@ -0,0 +1,18 @@
+# write a python code to demonstration set operations
+
+set1 = {1,2,3}
+set2 = {3,4,5}
+
+union = set1 | set2
+
+intersection = set1 & set2
+
+difference = set1 - set2
+
+print("Union:", union)
+print("Intersection:", intersection)
+print("Difference:", difference)
+
+print("\nThis program is written and executed by Mannan Tayal (0231BCA047)")
+
+#
