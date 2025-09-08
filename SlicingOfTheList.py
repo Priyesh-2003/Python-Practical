2b3aee0155570f2008360a5d4e956aac9250f27a
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 10:03:30 +0530

    Create SlicingOfTheList.py

---
+++ SlicingOfTheList.py
@@ -0,0 +1,17 @@
+#write a python code to demonstrate slicing of the list
+
+my_list = ['a','b','c','d','e']
+
+first_elemeent = my_list[0]
+
+last_element = my_list[-1]
+
+sub_list = my_list[1:4]
+
+print("First Element:",first_elemeent)
+
+print("Last Element:",last_element)
+
+print("Sliced List:",sub_list)
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
