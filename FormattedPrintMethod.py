5ef01625efcc2c49ae84334f7e76b823fb842b08
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:49:15 +0530

    Create FormattedPrintMethod.py

---
+++ FormattedPrintMethod.py
@@ -0,0 +1,14 @@
+# Taking input from user
+name = input("Enter your name: ")
+class_name = input("Enter your class: ")
+roll_no = input("Enter your roll number: ")
+age = input("Enter your age: ")
+
+# Displaying using formatted print
+print(f"\nStudent Details:")
+print(f"Name      : {name}")
+print(f"Class     : {class_name}")
+print(f"Roll No   : {roll_no}")
+print(f"Age       : {age}")
+
+print("This program is written and executed by Mannan Tayal (0231BCA047).")
