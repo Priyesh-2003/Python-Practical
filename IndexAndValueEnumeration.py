5127a15c7184f648852dfd5508605c4f0c2166e0
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:51:43 +0530

    Create IndexAndValueEnumeration.py

---
+++ IndexAndValueEnumeration.py
@@ -0,0 +1,9 @@
+print("Demonstration of Index and Value Enumeration:")
+# List of fruits
+fruits = ["apple", "banana", "cherry", "mango"]
+
+# Using enumerate to print index and value
+for index, value in enumerate(fruits):
+    print("Index:", index, "Value:", value)
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
