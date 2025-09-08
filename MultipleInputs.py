30017786fd36ff24bf77f302f7e16bc1e6139eca
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:55:01 +0530

    Create MultipleInputs.py

---
+++ MultipleInputs.py
@@ -0,0 +1,8 @@
+print("Demonstration of Mulitple Inputs:")
+# Taking multiple integer inputs from the user
+numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
+
+# Printing the inputs
+print("You entered:", numbers)
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
