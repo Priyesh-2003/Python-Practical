4cc68a385165e63c2ba6cf40f5489f85030f5b4e
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:44:34 +0530

    Create ContinueWithWhileLoop.py

---
+++ ContinueWithWhileLoop.py
@@ -0,0 +1,11 @@
+print("Demonstration of Continue Statement with While Loop:")
+# Print numbers from 1 to 10, skipping number 5
+
+i = 0
+while i < 10:
+    i += 1
+    if i == 5:
+        continue  # Skip the rest of the loop when i is 5
+    print(i)
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
