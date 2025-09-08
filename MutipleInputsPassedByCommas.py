4ed7f395586ac3aad30badabd8daa0bc46d368b0
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:55:28 +0530

    Create MutipleInputsPassedByCommas.py

---
+++ MutipleInputsPassedByCommas.py
@@ -0,0 +1,9 @@
+print("Demonstration of Multiple Inputs separated by Commas:")
+# Taking multiple integer inputs separated by commas
+numbers = list(map(int, input("Enter numbers separated by commas: ")
+                   .split(',')))
+
+# Printing the list of numbers
+print("List of numbers:", numbers)
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
