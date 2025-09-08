8318a7946cf382ffe36361a910d628575c38ebfc
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:42:23 +0530

    Create Built-inIterators.py

---
+++ Built-inIterators.py
@@ -0,0 +1,10 @@
+print("Program to USE Built-in ITERATORS:\n")
+
+my_list = [1,2,3,4]
+
+it = iter(my_list)
+
+print(next(it))
+print(next(it))
+
+print("\nThis program is written and executed by Mannan Tayal (0231BCA047)")
