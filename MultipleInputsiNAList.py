13c13a9c8928e2de965d4630cab0d8634ac4a91e
Priyesh kumar <priyeshgautam14@gmail.com>
2025-09-08 09:56:00 +0530

    Create MultipleInputsiNAList.py

---
+++ MultipleInputsiNAList.py
@@ -0,0 +1,8 @@
+print("Demonstration of Mulitple Inputs in a List:")
+# Taking multiple integer inputs and storing them in a list
+numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
+
+# Printing the list
+print("List of numbers:", numbers)
+
+print("This program is written and executed by Mannan Tayal (0231BCA047)")
